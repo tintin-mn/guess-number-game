25e21a3cdc74c23197cab0df8b6d1c696cc775b0
tina <tina1377mazikian@gmail.com>
2025-05-04 23:58:54 +0330

    add features: add lives, add toast instead alert fix: again button

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const input = document.querySelector('.input')
 const guessBtn = document.querySelector('.guess-btn')
 const guessForm = document.querySelector('.guess-form')
 const test = document.querySelector('.show-test')
+const againBtn = document.querySelector('.again-btn')
+const livesDisplay = document.querySelector('.lives')
 
 guessForm.addEventListener('click', function (e) {
     e.preventDefault()
@@ -11,18 +13,50 @@ guessBtn.addEventListener("click", function () {
     console.log(input.value, 'hey')
     const userGuess = input.value
     test.innerHTML = `${userGuess}`
+    input.value = ''
 })
 
-let myGuess = String(Math.round(Math.random() * 20))
+let lives = 5
+function updateLives() {
+    livesDisplay.innerHTML = '❤️'.repeat(lives)
+}
+
+function showToast(message) {
+    const toast = document.getElementById('toast')
+    toast.textContent = message
+    toast.classList.add('show')
+
+    setTimeout(() => {
+        toast.classList.remove('show')
+    }, 2000)
+}
+
+let myGuess = Math.round(Math.random() * 20)
 console.log(myGuess)
 
 function guessProcess () {
+    if ( Number(input.value) === myGuess ) {
+        showToast('you won 🎊')
+    } else {
+        lives--
+        updateLives()
 
-if ( input.value === myGuess ) {
-    alert('you won 🎊')
-} else if ( input.value > myGuess ) {
-    alert('the guessed number is too big ‼️')
-} else if ( input.value < myGuess ) {
-    alert('the guessed number is too small ‼️')
+        if (lives < 0) {
+            showToast('loosing, guess again')
+            playAgain()
+        } else {
+            showToast(Number(input.value) > myGuess ? 'Too Big' : 'Too Small')
+        }
+    }
 }
+
+function playAgain () {
+    againBtn.addEventListener('click', function () {
+        myGuess = Math.floor(Math.random() * 20) + 1
+        input.value = ''
+        test.innerHTML = ''
+        lives = 5
+        updateLives()
+        console.log(myGuess, 'new guess')
+    })
 }
