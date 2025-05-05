37a8014c88cf9158ffefe23bf380554915b430ae
tina <tina1377mazikian@gmail.com>
2025-05-05 13:32:05 +0330

    fix bugs todo: responsive css, fix bugs

--- main.js
+++ main.js
@@ -4,6 +4,16 @@ const guessForm = document.querySelector('.guess-form')
 const test = document.querySelector('.show-test')
 const againBtn = document.querySelector('.again-btn')
 const livesDisplay = document.querySelector('.lives')
+const toast = document.querySelector('.toast')
+const mainTitle = document.querySelector('.core-title')
+
+let lives = 5
+let myGuess = Math.round(Math.random() * 20) + 1
+console.log(myGuess)
+
+function updateLives() {
+    livesDisplay.innerHTML = '❤️'.repeat(lives)
+}
 
 guessForm.addEventListener('click', function (e) {
     e.preventDefault()
@@ -16,47 +26,48 @@ guessBtn.addEventListener("click", function () {
     input.value = ''
 })
 
-let lives = 5
-function updateLives() {
-    livesDisplay.innerHTML = '❤️'.repeat(lives)
+againBtn.addEventListener('click', function () {
+    myGuess = Math.floor(Math.random() * 20) + 1
+    test.innerHTML = ''
+    lives = 5
+    updateLives()
+    mainTitle.textContent = 'Start guessing...'
+    mainTitle.classList.remove('winText', 'loseText')
+    console.log(myGuess, 'new guess')
+})
+
+function guessProcess () {
+    if ( lives <= 0 ) {
+        mainTitle.textContent = "You lose! play again"
+        mainTitle.classList.add('loseText')
+        guessBtn.disabled = true
+        const theNumber = document.createElement('p')
+        theNumber.innerHTML = `the main number was ${myGuess}`
+    } else if ( Number(input.value) === myGuess ) {
+        mainTitle.textContent = 'You won! 🎊🎊🎊'
+        mainTitle.classList.add('winText')
+        guessBtn.disabled = true
+        input.disabled = true
+    } else if ( Number(input.value) < myGuess ) {
+        showToast('your guess is too Small')
+        lives--
+        updateLives()
+        input.value = ''
+        test.innerHTML = input.value
+    } else if ( Number(input.value) > myGuess ) {
+        showToast('your guess is too Big')
+        lives--
+        updateLives()
+        input.value = ''
+        test.innerHTML = input.value
+    }
 }
 
 function showToast(message) {
-    const toast = document.getElementById('toast')
     toast.textContent = message
     toast.classList.add('show')
 
     setTimeout(() => {
         toast.classList.remove('show')
-    }, 2000)
-}
-
-let myGuess = Math.round(Math.random() * 20)
-console.log(myGuess)
-
-function guessProcess () {
-    if ( Number(input.value) === myGuess ) {
-        showToast('you won 🎊')
-    } else {
-        lives--
-        updateLives()
-
-        if (lives < 0) {
-            showToast('loosing, guess again')
-            playAgain()
-        } else {
-            showToast(Number(input.value) > myGuess ? 'Too Big' : 'Too Small')
-        }
-    }
-}
-
-function playAgain () {
-    againBtn.addEventListener('click', function () {
-        myGuess = Math.floor(Math.random() * 20) + 1
-        input.value = ''
-        test.innerHTML = ''
-        lives = 5
-        updateLives()
-        console.log(myGuess, 'new guess')
-    })
+    }, 4000)
 }
